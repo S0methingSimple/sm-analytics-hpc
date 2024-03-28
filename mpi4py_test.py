bc2425187d2d33aeb7e496e6fb594c3a2cc9eaa1
S0methingSimple <30189981+S0methingSimple@users.noreply.github.com>
2024-03-29 01:24:03 +1100

    init: Initial commit

---
+++ mpi4py_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Program: mpi4py_test.py
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+inode = MPI.Get_processor_name()    # Node where this MPI process runs
+if iproc == 0: print ("This code is a test for mpi4py.")
+for i in range(0,nproc):
+    MPI.COMM_WORLD.Barrier()
+    if iproc == i:
+        print('Rank %d out of %d' % (iproc,nproc))
+MPI.Finalize()
